3e548f1ed934bb9ef911cf224ed060214ec5acc8
njain-01 <nj3510@gmail.com>
2020-11-30 22:59:15 +0530

    Demo.py updation

--- OLID/demo.py
+++ OLID/demo.py
@@ -22,6 +22,7 @@ else: # Add best model hardcoded below
     modelpathA=modelpathA+"log"
     modelpathB=modelpathB+"log"
     modelpathC=modelpathC+"log"
+
 modelA=load_model(modelpathA)
 modelB=load_model(modelpathB)
 modelC=load_model(modelpathC)
@@ -32,25 +33,26 @@ modelC=load_model(modelpathC)
 # print(modelA.predict(xA))
 # print(modelB.predict(xB))
 # print(modelC.predict(xC))
+
 while True:
     print("Please Enter Your Text Sentence: ")
     sent=input()
     xA=dataA.ui_helper([sent])
     outA=modelA.predict(xA)
     # print(outA[0])
-    print("Task A:")
+    print("Task A Model selected: ",modelpathA)
     if  outA[0]!=("OFF"):
         print("The given Sentence is not classified as Offensive")
     else:
         print("The given Sentence is classified as Offensive")
-        print("Task B:")
+        print("Task B Model selected: ",modelpathB)
         xB=dataB.ui_helper([sent])
         outB=modelB.predict(xB)
         if outB[0]!=("TIN"):
             print("The given Sentence is not classified as Targeted")
         else:
             print("The given Sentence is classified as Targeted")
-            print("Task C:")
+            print("Task C Model selected: ",modelpathC)
             xC=dataC.ui_helper([sent])
             outC=modelC.predict(xC)
             # print(outC)
@@ -64,3 +66,12 @@ while True:
     #     print("Task B:")
 	# print("Task B:")
 	# print("Task C:")
+"""
+Few Donal trump tweets examples to run from https://www.forbes.com/pictures/flji45elmm/9-dummy/?sh=4d27bc9578fb :
+
+How can a dummy dope like Harry Hurt, who wrote a failed book about me but doesn’t know me or anything about me, be on TV discussing Trump?
+
+My plan will lower taxes for our country, not raise them. Phony @club4growth says I will raise taxes—just another lie.
+
+Truly weird Senator Rand Paul of Kentucky reminds me of a spoiled brat without a properly functioning brain. He was terrible at DEBATE
+"""
